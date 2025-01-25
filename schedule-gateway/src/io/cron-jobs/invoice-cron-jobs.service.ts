8e583a1ebaa399e067d0b453c358a12b31eb40d7
nimamleo <nimamahini81@gmail.com>
2025-01-26 02:52:51 +0330

    refactor

--- schedule-gateway/src/io/cron-jobs/invoice-cron-jobs.service.ts
+++ schedule-gateway/src/io/cron-jobs/invoice-cron-jobs.service.ts
@@ -10,7 +10,7 @@ import { InvoiceService } from '@invoice/application/invoice/service/invoice.ser
 export class InvoiceCronJobsService {
   private readonly logger = new Logger(InvoiceCronJobsService.name);
   constructor(
-    // @Inject(EMAIL_WRITER) private readonly emailWriter: IEmailWriter,
+    @Inject(EMAIL_WRITER) private readonly emailWriter: IEmailWriter,
     private readonly invoiceService: InvoiceService,
   ) {}
 
@@ -22,17 +22,17 @@ export class InvoiceCronJobsService {
       return;
     }
 
-    // const res = await this.emailWriter.sendInvoiceEmail({
-    //   email: 'nimamahini81@gmail.com',
-    //   list: getSummary.value.map((x) => ({
-    //     totalSale: x.totalSale,
-    //     totalQuantity: x.totalQuantitySold,
-    //     sku: x.sku,
-    //   })),
-    // });
-    // if (res.isError()) {
-    //   this.logger.error('send summary failed');
-    // }
+    const res = await this.emailWriter.sendInvoiceEmail({
+      email: 'nimamahini81@gmail.com',
+      list: getSummary.value.map((x) => ({
+        totalSale: x.totalSale,
+        totalQuantity: x.totalQuantitySold,
+        sku: x.sku,
+      })),
+    });
+    if (res.isError()) {
+      this.logger.error('send summary failed');
+    }
     this.logger.debug('summary detail added to QUEUE');
   }
 }
--- schedule-gateway/src/io/io.module.ts
+++ schedule-gateway/src/io/io.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { InvoiceCronJobsService } from './cron-jobs/invoice-cron-jobs.service';
 import { ScheduleModule } from '@nestjs/schedule';
+import { CommandClientModule } from '../infrastructure/comand-client/command-client.module';
 import { InvoiceModule } from '@invoice/application/invoice/invoice.module';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), InvoiceModule],
+  imports: [ScheduleModule.forRoot(), CommandClientModule, InvoiceModule],
   providers: [InvoiceCronJobsService],
   exports: [InvoiceCronJobsService],
 })
